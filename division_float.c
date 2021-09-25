dee06b4c64de9674edf94373e253039f16c237d1
vadakkodan <pradeepvadakkodathu@gmail.com>
2021-09-25 21:07:23 +0530

    Division float result

---
+++ division_float.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+	float a,b;
+	float c;
+
+void main(){
+	printf("\nEnter two numbers:");
+	scanf("%f %f", &a, &b);
+	//c = a / b;
+	printf("%g",a/b);
+//	return 0;
+}
+
+//void main()
+//{
+////clrscr();
+//printf("\n Division operation\t\tResult");
+//printf("\n Two Integers (5 & 2) \t\t: %d",5/2);
+//printf("\n One int & one float (5.5 & 2)\t: %g",5.5/2);
+//printf("\n Two Integers (5 & 2)\t\t: %g",(float)5/2);
+//}
+//
+
