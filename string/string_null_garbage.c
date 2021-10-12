e18497b41c662703ee479a204d8dc72558427a1d
vadakkodan <pradeepvadakkodathu@gmail.com>
2021-10-12 10:47:59 +0530

    string concept %s and null character on gcc and clang is different

---
+++ string/string_null_garbage.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	char name [7] = "Pradeep";
+	char name1 [8] = "Pradeep";
+	printf("without null:\t %s\n",name);
+	printf("with null:\t %s\n",name1);
+	return 0;
+}
