98057d9fd146a895a49c1b4b82fa513f3b32062e
vadakkodan <pradeepvadakkodathu@gmail.com>
2021-11-01 12:49:49 +0530

    adding pointer dir

---
+++ pointer/ex19_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+int num;
+//clrscr();
+printf("Enter a Number =");
+scanf("%d",&num);
+printf("Value of num = %d\n",num);
+printf("Address of num=%p\n",&num);
+//getche();
+return 0;
+}
