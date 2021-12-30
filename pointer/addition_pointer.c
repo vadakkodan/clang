13835646a0d18aac72c6725ec47726d641e015ad
vadakkodan <pradeepvadakkodathu@gmail.com>
2021-12-31 00:00:57 +0530

    need to check only with pointers

--- pointer/addition_pointer.c
+++ pointer/addition_pointer.c
@@ -2,12 +2,21 @@
 #include <string.h>
 int main()
 {
-//int num1 = 0, num2 = 0, sum =0;
-int *nu1 ;
-int *nu2 ;
-int *sum ;
-printf("Numbers are = %d\t%d\t\n",*nu1,*nu2);
-printf("Addresses are = %p\t%p\t%p\n",nu1,nu2,sum);
+	int n1=0, n2 = 0, sum = 0;
+	int *nu1 ;
+        int *nu2 ;
+        int *summ ;
+	nu1 = &n1;
+	nu2 = &n2;
+	summ = &sum;
+        printf ("Enter 2 Numbers: ");
+        scanf ("%d %d", &n1,&n2);
+        printf("Numbers are = %d\t%d\n",*nu1,*nu2);
+        printf("Addresses are = %p\t%p\n",nu1,nu2);
+	*summ = *nu1 + *nu2 ;
+        printf("Numbers are = %d\t%d\t%d\n",*nu1,*nu2,*summ);
+        //printf("Addresses are = %p\t%p\n",nu1,nu2);
 
-return 0;
+        
+        return 0;
 }
