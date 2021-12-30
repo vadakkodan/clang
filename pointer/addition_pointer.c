c3fee7d9f5d7e22a2d4e1db859b6693b1f2ed73b
vadakkodan <pradeepvadakkodathu@gmail.com>
2021-12-30 22:53:22 +0530

    relearn git

--- pointer/addition_pointer.c
+++ pointer/addition_pointer.c
@@ -2,24 +2,12 @@
 #include <string.h>
 int main()
 {
-
 //int num1 = 0, num2 = 0, sum =0;
-int *nu1 = 0 ;
-int *nu2 = 0 ;
-int *sum = 0;
-//nu1 = &num1;
-//nu2 = &num2;
-printf("Enter two Number =");
-scanf("%d %d",nu1,nu2);
+int *nu1 ;
+int *nu2 ;
+int *sum ;
 printf("Numbers are = %d\t%d\t\n",*nu1,*nu2);
 printf("Addresses are = %p\t%p\t%p\n",nu1,nu2,sum);
-//*sum = *nu1 + *nu2;
 
-printf("Numbers are = %d\t%d\t%d\n",*nu1,*nu2,*sum);
-printf("Addresses are = %p\t%p\t%p\n",nu1,nu2,sum);
-//printf("Ad=%d\n",num1);
-//printf("Address of num=%p\n",&num);
-//printf("Address of num1=%p\n",&num1);
-//getche();
 return 0;
 }
