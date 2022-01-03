d8a3974bfb49d37cb91a81b0146f7968820dd842
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-01-03 16:39:39 +0530

    address of each element in a array using pointer

---
+++ pointer/array_pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a[5] = {1,2,3,4,5};
+	int *p;
+	p = &a[0];
+
+	int i = 0;
+	for (i=0;i<5;i++) {
+		printf ("Value is %d and address is %p\n", *p,p);
+		p++ ;
+	}
+	printf("\n");
+	return 0;
+}
+
