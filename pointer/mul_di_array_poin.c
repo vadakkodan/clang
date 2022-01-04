529863ae4ac9ff4991f1765758c2e028f4900429
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-01-04 15:08:59 +0530

    tested with for loop and 3x3 matrix

--- pointer/mul_di_array_poin.c
+++ pointer/mul_di_array_poin.c
@@ -2,17 +2,12 @@
 #include <stdlib.h>
 int main() {
 	int i = 0,j;
-	int a[2][2] = { {1,2},{3,4} };
-//	printf ("%d", a[1][1]); 	
-        while (i<2) {
-//		printf ("%d",i);
-	for(j=0;j<2;j++) {
-		printf("%d\t",a[i][j]);
-	//	printf("%d",i);
-	//	j++;
-	}
-	printf ("\n");
-	i++;
+	int a[3][3] = { {1,2,3},{4,5,6} , {7,8,9}};
+	for (i=0;i<3;i++) {
+		for (j=0;j<3;j++) {
+			printf("%d\t",a[i][j]);
+		}
+		printf ("\n");
 	}
 	return 0;
 }
