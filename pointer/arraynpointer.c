e163d068567ed8222cdd3bcbd734940fada7935c
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-01-07 22:46:00 +0530

    changes

--- pointer/arraynpointer.c
+++ pointer/arraynpointer.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main() {
-	int a=5;
-//	int a[3]= {1,2,3},i;
+	int a[3]= {1,2,3},i;
 	int *p;
-	p = &a;
-	printf ("%p %d",p,*p);
-
+	p = &a[0];
+	
 //	int *ptr[3];
 //	ptr = a[0];
 //	ptr+1 = a[1];
 //	ptr+2 = a[2];
 //
-//	for (i=0;i<3;i++) {
-//		printf ("%d %p", *ptr[i],ptr); }
+	for (i=0;i<3;i++) {
+		printf ("%d %p\n", *p,p);
+	p++;	
+	}
 	return 0;
 }
 
