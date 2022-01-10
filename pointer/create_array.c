e9ee9423f0cbafa6417c623798ec43fcc48778ef
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-01-10 17:33:01 +0530

    understanding 2d array

--- pointer/create_array.c
+++ pointer/create_array.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 int main() {
-	int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-	int *p;
-	p = &a[0][0];
-	printf("element: %d\t Address: %p\n ",(*(p+2)+2),((p+2)+2));
+	int a[3][3] = {{9,2,3},{4,5,6},{7,8,9}};
+	int i;
+	printf ("%p %d\n",*a+0,*(*a+0));
+	
+//	int *p;
+//	p = a;
+//	printf("%d %p",*p,p);
+	
+	
+//	int i,j;
+//	p = &a[0][0];
+//	for (i=0;j=0;i<3;j<3;i++;j++){
+//		printf("Element:%d Address:%p\n",*((p+i)+j),((p+i)+j));
+//		}
+	
+	//for (i=0;i<3;i++) {
+	//	for (j=0;j<3;j++) {
+	//		printf("element: %d\t Address: %p\n ",(*(p+i)+j),((p+i)+j));
+	//		printf("element: %d\t Address: %p\n ",a[i][j], &(a[i][j]));
+	//	}
+	//}
 	return 0;
 }
