21273147d2ac194b0d6a7cb9cd43f187118f926b
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-01-18 16:01:19 +0530

    addition using pointer

---
+++ hackerrank/pointerinc.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void update(int *a,int *b) {
+	int max = *a+*b;
+	int diff= abs(*a-*b);
+	*a = max;
+	*b = diff;
+    
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
