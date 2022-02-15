d17722b9aa0269033df84a6ac981bf4626ab44c5
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-02-15 22:32:27 +0530

    func call by value experiment

---
+++ function/by_value_swap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int swap(int x, int y);
+int main() {
+	int a,b;
+	printf("Enter two numbers: \n");
+	scanf("%d %d",&a,&b);
+	swap(a,b);
+	printf("Numbers after swap function,expect no change: %d %d\n",a,b);
+	return 0;
+}
+int swap(int x,int y) {
+	int tmp=0;
+	tmp = x;
+	x=y;
+	y=tmp;
+	printf("Inside the swap function a and b values: %d %d\n", x,y);
+	return 0;
+}
+
+
+
