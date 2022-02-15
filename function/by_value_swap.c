29d2a19f33e78a8de028c46999d14a1af04cc6e8
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-02-15 23:58:50 +0530

    actually ref

--- function/by_value_swap.c
+++ function/by_value_swap.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
-int swap(int x, int y);
+int swap(int *x, int *y);
 int main() {
 	int a,b;
 	printf("Enter two numbers: \n");
 	scanf("%d %d",&a,&b);
-	swap(a,b);
+	swap(&a,&b);
 	printf("Numbers after swap function,expect no change: %d %d\n",a,b);
 	return 0;
+	       
 }
-int swap(int x,int y) {
+int swap(int *x,int *y) {
 	int tmp=0;
-	tmp = x;
-	x=y;
-	y=tmp;
-	printf("Inside the swap function a and b values: %d %d\n", x,y);
+	tmp = *x;
+	*x=*y;
+	*y=tmp;
+	printf("Inside the swap function a and b values: %d %d\n", *x,*y);
 	return 0;
 }
 
